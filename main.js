d9a528f67d36a2cfb7b6fa4813450d57161c87f0
Sofia Ribeiro <sofia.ribeiro@creditas.com>
2022-11-10 15:43:37 -0300

    feat: insertion of playClap

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ function playPom(){
     document.querySelector('#som_tecla_pom').play(); 
 }
 
+function playClap(){
+    document.querySelector('#som_clap').play();
+}
+
 document.querySelector('.tecla_pom');
+document.querySelector('.tecla_clap');
