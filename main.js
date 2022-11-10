eb7105b15712e4b5be607810d1f1afde77a3b3ab
Sofia Ribeiro <sofia.ribeiro@creditas.com>
2022-11-10 17:54:08 -0300

    feat: insert onclick playpom

--- main.js
+++ main.js
@@ -2,44 +2,6 @@ function playPom(){
     document.querySelector('#som_tecla_pom').play(); 
 }
 
-function playClap(){
-    document.querySelector('#som_clap').play();
-}
+document.querySelector('.tecla_pom').onclick = playPom;
 
-function playTim(){
-    document.querySelector('#som_tecla_tim').play();
-}
 
-function playPuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-
-function playSplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-
-function playToim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-
-function playPsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-
-function playTic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-
-function playTom(){
-    document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_pom');
-document.querySelector('.tecla_clap');
-document.querySelector('.tecla_tim');
-document.querySelector('.tecla_puff');
-document.querySelector('.tecla_splash');
-document.querySelector('.tecla_toim');
-document.querySelector('.tecla_psh');
-document.querySelector('.tecla_tic');
-document.querySelector('.tecla_tom');
