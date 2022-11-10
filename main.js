4f5ec7e7c064504d0a3ffda929e930f026b193da
Sofia Ribeiro <sofia.ribeiro@creditas.com>
2022-11-10 15:46:47 -0300

    feat: insert play puff

--- main.js
+++ main.js
@@ -10,6 +10,11 @@ function playTim(){
     document.querySelector('#som_tecla_tim').play();
 }
 
+function playPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
 document.querySelector('.tecla_pom');
 document.querySelector('.tecla_clap');
 document.querySelector('.tecla_tim');
+document.querySelector('.tecla_puff');
